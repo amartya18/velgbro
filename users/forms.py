5a9d3da52b229da9cbc7ebd8b2c07596c272107d
marcantoniosmap <marcantoniosmap@gmail.com>
2019-12-25 22:04:35 +0700

    beautifuerer

--- users/forms.py
+++ users/forms.py
@@ -3,11 +3,17 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30)
-    last_name = forms.CharField(max_length=30)
-    email = forms.EmailField(max_length=200)
-    phone_number = forms.CharField(max_length=30)
+    first_name = forms.CharField(max_length=30,widget=forms.TextInput(attrs={'placeholder': 'First Name'}))
+    last_name = forms.CharField(max_length=30,widget=forms.TextInput(attrs={'placeholder': 'Last Name'}))
+    email = forms.EmailField(max_length=200,widget=forms.TextInput(attrs={'placeholder': 'E-mail'}))
+    phone_number = forms.CharField(max_length=30,widget=forms.TextInput(attrs={'placeholder': 'Phone-number'}))
+    password1=forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+    password2=forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Repeat Password'}))
 
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'phone_number', 'email', 'password1', 'password2')
+        widgets={
+            
+            'username': forms.TextInput(attrs={'placeholder':'Username'}),
+        }
